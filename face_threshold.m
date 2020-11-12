2cfcfabdcb8b0f1599a423cb889417d66a97e210
Viktor <34343728+viksj950@users.noreply.github.com>
2020-11-12 13:29:47 +0100

    Added a threshold parameter

--- face_threshold.m
+++ face_threshold.m
@@ -1,11 +1,11 @@
-function [thresholded] = face_threshold(face)
+function [thresholded] = face_threshold(face, t_value)
 %Thresholds the eyeMap
 [rows, cols] = size(face);
 thresholded = face;
 
 for row = 1:rows
     for col = 1:cols
-        if(face(row,col) > 210)
+        if(face(row,col) > t_value)
             thresholded(row,col) = 255;
         else
             thresholded(row,col) = 0;
