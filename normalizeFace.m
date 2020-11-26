bd34ce93ed037a12ce2501fca3652631834ffedb
Algot Sandahl <algot.sandahl@gmail.com>
2020-11-26 10:02:04 +0100

    Fix normalizeFace

--- normalizeFace.m
+++ normalizeFace.m
@@ -4,39 +4,15 @@ function normalizedImage = normalizeFace(image)
 
 [leftEye, rightEye, mouth] = get_eye_mouth_coord(image);
 
-eyeLineVec = [rightEye - leftEye 0];
-eyeLineVec = eyeLineVec ./ norm(eyeLineVec);
-offsetAngle = atan2(norm(cross(eyeLineVec, [1 0 0])), dot(eyeLineVec, [1 0 0]));
+imageSize = [400, 300];
+fixedLeftEye = [100 100];
+fixedRightEye = [200 100];
+fixedMouth = [150 300];
+fixedPositions = [fixedLeftEye; fixedRightEye; fixedMouth];
 
-leftEye = round(leftEye);
-rightEye = round(rightEye);
-mouth = round(mouth);
+transformation = fitgeotrans([leftEye; rightEye; mouth], fixedPositions, 'similarity');
+normalizedImage = imwarp(image, transformation,'OutputView',imref2d(imageSize));
 
-% Size of scaled image
-scaleY = 400;
-scaleX = 300;
-% Find scale factor to normalize eye position
-targetEyesDist = 140; % This generally works well
-distBetweenEyes = rightEye(1) - leftEye(1);
-scaleFactor = targetEyesDist / distBetweenEyes;
-image = imresize(image, scaleFactor, 'bicubic');
-
-% Rescale all values
-leftEye = round(leftEye .* scaleFactor);
-rightEye = round(rightEye .* scaleFactor);
-mouth = round(mouth .* scaleFactor);
-
-topmostEye = min(leftEye(2), rightEye(2)); % Highest eye point
-distEyesMouth = round((mouth(2) - topmostEye));
-marginX = round((scaleX - distBetweenEyes)/2);
-
-marginYtotal = scaleY - distEyesMouth;
-marginYbot = round(0.35*marginYtotal);
-marginYtop = round(0.65*marginYtotal);
-
-rotatedImage = imrotate(image, offsetAngle * 180 / pi, 'crop', 'bicubic');
-normalizedImage = rotatedImage(topmostEye-marginYtop:mouth(2)+marginYbot, leftEye(1)-marginX:rightEye(1)+marginX, :);
-normalizedImage = imresize(normalizedImage, [scaleY scaleX]);
 % Use grayscale image for now, might use something else later
 normalizedImage = rgb2gray(normalizedImage);
 end
